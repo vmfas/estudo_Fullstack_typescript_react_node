419b4cbab81aa41539d8d39e1edacb8066520123
vmfas <124852156+vmfas@users.noreply.github.com>
2023-06-21 00:51:17 -0300

    att

---
+++ app.ts
@@ -0,0 +1,6 @@
+const soma = (a: number, b:number) => {
+    console.log(a + b)
+}
+
+soma(1, 2)
+soma(4, 2)
---
+++ index.js
@@ -0,0 +1,6 @@
+const soma = (a, b) => {
+    console.log(a + b)
+}
+
+soma(1, 2)
+soma('vinicius', ' Miguel')
